e0ae247d2f63b9b0b55ab2d1714060d5e876a6b1
jeereq <mingandajeereq@gmail.com>
2023-02-13 19:54:05 +0100

    refactor: update title of message in section contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -58,7 +58,7 @@ export const Contact = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <h2>Ecris moi</h2>
+                  <h2>Ecrivez moi</h2>
                   <form onSubmit={handleSubmit}>
                     <Row>
                       <Col size={12} sm={6} className="px-1">
